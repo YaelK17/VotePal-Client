a2ce7f5d0a011c0a25258a289d4a34c40b417043
YaelK17 <46296@guttman.ort.org.il>
2024-01-19 10:27:34 +0200

    added variable

--- app/src/main/java/com/example/openingscreen/TestActivity.java
+++ app/src/main/java/com/example/openingscreen/TestActivity.java
@@ -3,8 +3,10 @@ package com.example.openingscreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TestActivity extends AppCompatActivity {
+    TextView a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
