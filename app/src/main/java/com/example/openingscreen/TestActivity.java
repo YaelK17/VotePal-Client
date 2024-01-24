3d477428d794288628f47a19a9649c97764a74e5
YaelK17 <46296@guttman.ort.org.il>
2024-01-24 14:15:23 +0200

    added gotosignup in TestActivity

--- app/src/main/java/com/example/openingscreen/TestActivity.java
+++ app/src/main/java/com/example/openingscreen/TestActivity.java
@@ -1,16 +1,29 @@
 package com.example.openingscreen;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class TestActivity extends AppCompatActivity {
-    TextView a;
+    TextView gotosignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        gotosignup = findViewById(R.id.gotosignup);
+
+        gotosignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TestActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
