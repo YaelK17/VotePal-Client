a8d9791d393476b8e6c4887553e015a867221496
YaelK17 <46296@guttman.ort.org.il>
2024-03-29 14:31:40 +0300

    added unversioned files

---
+++ app/src/main/java/com/example/openingscreen/Client.java
@@ -0,0 +1,45 @@
+package com.example.openingscreen;
+
+import android.widget.Toast;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.inject.Singleton;
+import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+
+public class Client {
+   private static Client client_instance = null;
+   private Socket socket;
+   private DataInputStream din;
+   private DataOutputStream dout;
+
+    private Client() throws IOException {
+        socket=new Socket("10.0.2.2", 1234);
+        dout = new DataOutputStream(socket.getOutputStream());
+        din = new DataInputStream(socket.getInputStream());
+    }
+
+    public static synchronized Client getClient_instance() throws IOException {
+        if (client_instance == null) {
+            client_instance = new Client();
+        }
+        return client_instance;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+    public DataOutputStream getdout(){
+        return dout;
+    }
+    public DataInputStream getdin(){
+        return din;
+    }
+
+    //DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+    //DataInputStream dIn = new DataInputStream(socket.getInputStream());
+}
