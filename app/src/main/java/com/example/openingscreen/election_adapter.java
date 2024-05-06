7b4afecc3d3a319926b5a3cbc8fd758bac70244f
YaelK17 <46296@guttman.ort.org.il>
2024-05-06 18:34:08 +0300

    added alert at delete

--- app/src/main/java/com/example/openingscreen/election_adapter.java
+++ app/src/main/java/com/example/openingscreen/election_adapter.java
@@ -17,6 +17,8 @@ import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
+import android.content.DialogInterface;
+import androidx.appcompat.app.AlertDialog;
 public class election_adapter extends ArrayAdapter<election_details>{
     private Context mcontext;
     private int mResource;
@@ -39,14 +41,52 @@ public class election_adapter extends ArrayAdapter<election_details>{
         election_title.setText(getItem(position).getElection_name());
         due_date.setText(getItem(position).getDue_date());
 
-        // Set OnClickListener on photoImageView
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(mcontext, "election_option selected: " + getItem(position).getElection_name(), Toast.LENGTH_SHORT).show();
+        // Set OnClickListener on photoImageView- only if its delete image
+        if (getItem(position).getImage() == R.drawable.baseline_delete_24) {
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Create the object of AlertDialog Builder class
 
-            }
-        });
+                    AlertDialog.Builder builder = new AlertDialog.Builder(mcontext);
+
+                    // Set the message show for the Alert time
+                    builder.setMessage("Are you sure you want to delete " + getItem(position).getElection_name() + "?");
+
+                    // Set Alert Title
+                    builder.setTitle("Alert !");
+
+                    // Set Cancelable false for when the user clicks on the outside the Dialog Box then it will remain show
+                    builder.setCancelable(false);
+
+                    // Set the positive button with yes name Lambda OnClickListener method is use of DialogInterface interface.
+                    builder.setPositiveButton("Yes", (DialogInterface.OnClickListener) (dialog, which) -> {
+
+                        // When the user click yes button then app will close
+
+                        // todo send to sever the delete
+
+                        Toast.makeText(mcontext, "deleted " + getItem(position).getElection_name(), Toast.LENGTH_SHORT).show();
+
+                    });
+
+                    // Set the Negative button with No name Lambda OnClickListener method is use of DialogInterface interface.
+                    builder.setNegativeButton("No", (DialogInterface.OnClickListener) (dialog, which) -> {
+
+                        // If user click no then dialog box is canceled.
+
+                        dialog.cancel();
+
+                    });
+
+                    // Create the Alert dialog
+                    AlertDialog alertDialog = builder.create();
+
+                    // Show the Alert Dialog box
+                    alertDialog.show();
+                }
+            });
+        }
 
         return convertView;
     }
